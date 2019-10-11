1ed8f153f2fec428f602977ce070356d89d57b25
stefangraf <stefan.graf@talent-factory.ch>
2019-10-11 16:15:26 +0200

    Model-Korrekturen / Erfassen des ersten Rooms

--- main/models.py
+++ main/models.py
@@ -4,17 +4,19 @@ from django.utils import timezone
 
 
 # Channel transmission modes are:
-class TransmissionMode(Enum):
-    SIMPLEX = 'Simplex'
-    HALF_DUPLEX = 'Half-Duplex'
-    DUPLEX = 'Duplex'
+TransmissionMode = (
+    ('SIMPLEX', 'Simplex'),
+    ('HALF_DUPLEX', 'Half-Duplex'),
+    ('DUPLEX', 'Duplex'),
+)
 
 
 # Create your models here.
 class Room(models.Model):
     room_name = models.CharField(max_length=80, name='room_name', blank=False)
     room_id = models.CharField(max_length=50, name='room_id', blank=False)
-    trans_mode = models.CharField(max_length=20, name='trans_mode', choices=TransmissionMode, default=TransmissionMode.DUPLEX)
+    trans_mode = models.\
+        CharField(max_length=20, name='trans_mode', choices=TransmissionMode, default=TransmissionMode[2])
     remark = models.TextField(name='remark', blank=True, null=True)
     created = models.DateTimeField(name='created', default=timezone.now)
 
