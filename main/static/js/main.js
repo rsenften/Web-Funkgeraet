dee2428c1b5bc91ad7d0a9a364659bdc9cc7fd1b
yasinfedail <yasin.fedail@89grad.ch>
2019-10-15 16:15:06 +0200

    Implemented prototype of volume and squelch regulation; Extended code documentation; Added real functionality to some of the buttons;

--- main/static/js/main.js
+++ main/static/js/main.js
@@ -1,19 +1,31 @@
+// ----------------------------------------------------------------------
+// DEVICE CLASS DEFINITION
+
 class Device {
     constructor() {
         this.device_id = "0123456789";
-        this.current_channel = 0;
+        this.current_channel = 16;
         this.coordinate_N = '25°48N';
         this.coordinate_W = '80°12W';
         this.country = 'USA';
         this.power = '25W';
         this.stand_by = true;
         this.time = '12:00';
+        this.volume = 5;
+        this.squelch = 5;
         this.render_display();
     }
 
 
     render_display() {
-        $('#channel').html(Math.round(this.current_channel));
+        let channel = Math.round(this.current_channel);
+
+        if ((channel+ "").length == 1) { // if channel number only has one digit, add 0 (e.g: 1 => '01')
+
+            channel = '0'+channel;
+        };
+        $('#channel').html(channel);
+
         $('#time').html(this.time);
         $('#coordinate_N').html(this.coordinate_N);
         $('#coordinate_W').html(this.coordinate_W);
@@ -25,39 +37,120 @@ class Device {
         $('#btn_4_description').html("HI/LO");
         $('#standby').html("STBY");
     }
+
+    render_vol_dialogue() {
+        let bar_width = Math.round(this.volume)*5;
+        $('#vol_number').html(Math.round(this.volume));
+        $('#vol_dialogue').css('display', 'inline');
+        $('#vol_bar_progress').css('width', bar_width+'%');
+        this.hide_sql_dialogue();
+        $('#display_left_col, #display_right_col').css('visibility', 'hidden');
+        $('#display_left_col, #display_right_col').css('display', 'none');
+
+    }
+
+    hide_vol_dialogue() {
+        $('#vol_dialogue').css('display', 'none');
+        $('#display_left_col, #display_right_col').css('visibility', 'visible');
+        $('#display_left_col, #display_right_col').css('display', 'block');
+    }
+
+    render_sql_dialogue() {
+        let bar_width = Math.round(this.squelch)*5;
+        $('#sql_number').html(Math.round(this.squelch));
+        $('#sql_dialogue').css('display', 'inline');
+        $('#sql_bar_progress').css('width', bar_width+'%');
+        this.hide_vol_dialogue();
+        $('#display_left_col, #display_right_col').css('visibility', 'hidden');
+        $('#display_left_col, #display_right_col').css('display', 'none');
+
+    }
+
+    hide_sql_dialogue() {
+        $('#sql_dialogue').css('display', 'none');
+        $('#display_left_col, #display_right_col').css('visibility', 'visible');
+        $('#display_left_col, #display_right_col').css('display', 'block');
+    }
+
 }
+
+var device = new Device();
+
 // ----------------------------------------------------------------------
+// BUTTON EVENT LISTENERS
 
 $('#btnScan, #btnDW, #btnCH_WX, #btnHI_LO, #btn_16_c').mousedown( function() {
-    $(this).css({'width': '90%',
-                 'height': '90%'});
+    $(this).css({'width': '95%',
+                 'height': '95%'});
 });
 
 $('#btnScan, #btnDW, #btnCH_WX, #btnHI_LO, #btn_16_c').mouseup(function() {
     $(this).css({'width': '100%',
                  'height': '100%'});
-    let color = '#'+(Math.random()*0xFFFFFF<<0).toString(16);
-    $('body').css({'background-color': color});
+});
+
+$('#btnHI_LO').click(function () { // This soft key toggles the device power
+    if (device.power == '1W') {
+        device.power = '25W';
+    }
+
+    else {
+        device.power = '1W';
+    };
+    device.render_display();
+});
+
+$('#btn_16_c').click(function () { // This changes the current channel to 16
+    device.current_channel = 16;
+    device.render_display();
 });
 
 // ----------------------------------------------------------------------
+// REGULATION OF VOLUME AND SQUELCH (USING THE CONTROL WHEEL)
 
-var device = new Device();
+// The mechanism does not yet work exactly as specified in the device manual. ==> Evaluate with Daniel
 
-installRotateOf('control_wheel_img', function (cr, lastAngle) {
-    if(cr < lastAngle){
-        device.current_channel -= 0.1;
-    }
-    else {
-        device.current_channel += 0.1;
-    };
 
-    if(device.current_channel >= 68){
-        device.current_channel = 0;
-    };
-    if(device.current_channel < 0){
-        device.current_channel = 68;
-    };
+// toggles visibility of squelch/volume dialogues (depending on the amount of clicks (1 or 2))
+$('#control_wheel_img').mousedown(function (e) {
 
-    device.render_display();
+      if(e.originalEvent.detail == 2){
+        device.hide_vol_dialogue();
+        device.render_sql_dialogue();
+        return;
+      };
+        device.hide_sql_dialogue();
+        device.render_vol_dialogue();
+
 });
+
+// hides squelch and volume dialogues as soon as the mouse is released (with a delay of 500ms)
+$('body').mouseup(function (e) {
+    setTimeout(function () {
+    device.hide_sql_dialogue();
+    device.hide_vol_dialogue();
+    }, 500)
+});
+
+
+// call the installRotateOf function (located in wheel_mechanics.js) and defines its handler
+installRotateOf('control_wheel_img', function (cr, lastAngle) {
+
+    let sensitivity_coefficient = 0.1; // this value regulates the sensitivity of the control wheel
+
+    if ($('#vol_dialogue').css('display') == 'none') { // if the vol_dialogue is not displayed, regulate squelch
+
+        if(cr < lastAngle && device.squelch > 0) {device.squelch -= sensitivity_coefficient;};
+
+        if(cr > lastAngle && device.squelch < 20) {device.squelch += sensitivity_coefficient;};
+
+        device.render_sql_dialogue();}
+    
+    else { // if the sql_dialogue is not displayed, regulate volume
+
+        if(cr < lastAngle && device.volume > 0) {device.volume -= sensitivity_coefficient;};
+
+        if(cr > lastAngle && device.volume < 20) {device.volume += sensitivity_coefficient;};
+
+        device.render_vol_dialogue();}
+    });
